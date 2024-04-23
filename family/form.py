7b6bc3cdc17a88e811b8877edb74572570e4e538
douglas <douglasmaxton@gmail.com>
2024-04-23 15:56:28 +0100

    changed: birth_date to only do from current date

---
+++ family/form.py
@@ -0,0 +1,24 @@
+import datetime
+
+from django import forms
+from .models import Child
+
+
+class ChildForm(forms.ModelForm):
+    """
+    Form for creating and updating child objects
+    """
+
+    class Meta:
+        model = Child
+        fields = ['child_name', 'birth_date']
+        widgets = {'birth_date': forms.widgets.DateTimeInput(attrs={'type': 'datetime-local'})}
+
+    def __init__(self, *args, **kwargs):
+        super(ChildForm, self).__init__(*args, **kwargs)
+
+        # sets validation for time_slots field
+        self.fields['birth_date'].required = True
+        # this sets a validation to enter a date more than 1 hour from now
+        self.fields['birth_date'].widget.attrs['min'] = (
+                datetime.datetime.now() + datetime.timedelta(hours=1)).strftime('%Y-%m-%d %H:%M')
--- family/views.py
+++ family/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .form import ChildForm
 from .models import Child
 
 
 # Create your views here.
-class FamilyListView(LoginRequiredMixin, ListView):
+class FamilyListView(LoginRequiredMixin, FormView):
     template_name = 'family/index.html'
     model = Child
+    form_class = ChildForm
 
     def get_context_data(self, **kwargs):
         context = super(FamilyListView, self).get_context_data(**kwargs)
 
         context['name'] = ['family']
 
-        return {'context': context}
+        return {'context': context, 'form': self.form_class()}
