03f683742d059a5904e85dda969835fff7610c1f
douglas <douglasmaxton@gmail.com>
2024-04-23 11:36:05 +0100

    added: bootstrap with base and index.html

--- home/views.py
+++ home/views.py
@@ -9,6 +9,6 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
 
-        context['name'] = 'title'
+        context['name'] = ['title']
 
         return {'context': context}
