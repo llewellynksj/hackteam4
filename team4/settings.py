1bbe1a74ed59fbfd9f0e99899224105cb18a8251
Kate L <llewellyn.ksj@gmail.com>
2024-04-23 10:09:06 +0100

    Update settings for deployment

--- team4/settings.py
+++ team4/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 if os.path.isfile("env.py"):
     import env
@@ -76,10 +77,16 @@ ASGI_APPLICATION = 'team4.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.parse(os.environ.get('DJANGO_DATABASE_URL'))
     }
 }
 
