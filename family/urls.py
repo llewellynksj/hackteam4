732173826568a568b9fda1cc476235d9f740d655
douglas <douglasmaxton@gmail.com>
2024-04-23 16:11:14 +0100

    added: form_valid method to save to db

--- family/urls.py
+++ family/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import FamilyListView
+from .views import FamilyListView, FamilyCreateView
 
 app_name = 'family'
 
 urlpatterns = [
     path('', FamilyListView.as_view(), name='family'),
+    path('create/', FamilyCreateView.as_view(), name='family_create'),
 ]
--- family/views.py
+++ family/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import ListView, FormView
+from django.views.generic import ListView, FormView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 
 from .form import ChildForm
 from .models import Child
 
 
 # Create your views here.
+class FamilyCreateView(LoginRequiredMixin, CreateView):
+    model = Child
+    form_class = ChildForm
+    message = 'You have successfully created a child to your family.'
+
+    def form_valid(self, form):
+        """
+        Once form is valid save to a database
+        """
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = self.request.user
+            return redirect('family:family')
+        return super().form_valid(form)
+
+
 class FamilyListView(LoginRequiredMixin, FormView):
     template_name = 'family/index.html'
     model = Child
