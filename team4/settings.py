cee5a45760587e9aef64bd8877f23f29ebe60430
douglas <douglasmaxton@gmail.com>
2024-04-23 09:14:07 +0100

    added: 3 constant variables for allauth

--- team4/settings.py
+++ team4/settings.py
@@ -47,6 +47,10 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 ]
 
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
