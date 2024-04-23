6870a74096a7a693ab55b8f11e44dcd2db212f46
Kate L <llewellyn.ksj@gmail.com>
2024-04-23 10:15:17 +0100

    Add allowed host

--- team4/settings.py
+++ team4/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "https://hackteam-4-b8cd4765f17b.herokuapp.com/"]
 
 # Application definition
 
