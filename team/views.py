fccb36acbe11d30e053e31e7e25c458d3a87ce6c
James Evans <james_e75@hotmail.com>
2024-04-25 13:04:28 +0000

    Add team page template, urls and views

--- team/views.py
+++ team/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+from django.shortcuts import render
 
 
-def team(request):
-    return HttpResponse("Hello, world. You're at the team page.")
+# Create your views here.
+class TeamView(TemplateView):
+    template_name = 'team/team.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TeamView, self).get_context_data(**kwargs)
+
+        context['name'] = ['title']
+
+        return {'context': context}
