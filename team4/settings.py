fb533303bbe7165eaa18980b057f50259aca6c51
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-04-26 14:02:27 +0000

    commit

--- team4/settings.py
+++ team4/settings.py
@@ -150,11 +150,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STRORAGES = [
+
+STORAGES = {
+    # ...
     "staticfiles": {
         "BACKEND": "cloudinary_storage.storage.StaticHashedCloudinaryStorage",
     },
-]
+}
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
