f594b7d598640b1139f5f1dda61b8f4120caf020
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-04-26 13:41:05 +0000

    Update static setting

--- team4/settings.py
+++ team4/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'static'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
